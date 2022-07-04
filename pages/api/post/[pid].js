db26268f574e24a1f6938858177c1faf1a270700
ka2ki <ka2ki.7na.splovers@gmail.com>
2022-07-05 01:37:45 +0900

    initialCommit

---
+++ pages/api/post/[pid].js
@@ -0,0 +1,6 @@
+// Dynamic API Routes
+// https://nextjs.org/docs/api-routes/dynamic-api-routes
+export default function handler(req, res) {
+  const { pid } = req.query;
+  res.end(`Post: ${pid}`);
+}
